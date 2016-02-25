6b8b213f4b2301fd80594e75de2073ce63bb5c68
Weirch Sodora <sodora@gmail.com>
2016-02-25 17:01:07 +0800

    modify search_engine.py

--- scripts/search_engine.py
+++ scripts/search_engine.py
@@ -10,21 +10,21 @@ search_engines = readcsvfile(search_engine_file)
 stock_lists_file = 'csv/stock_lists.csv'
 stock_lists = readcsvfile(stock_lists_file)
 
-daily_out_file = 'csv/daily_search_baidu.csv'
+sqlite_file = 'sqlite/search_engine.db'
 
-results = []
+conn = sqlite3.connect(sqlite_file)
+
 for search_engine in search_engines:
     for stock in sorted(stock_lists):
         url = search_engines[search_engine][0] % ('"' + stock_lists[stock][0] + '"')
         patten = search_engines[search_engine][1]
         print "%s,%s,%s" % (search_engine,stock,stock_lists[stock][0])
         response = urllib2.urlopen(url)
-        time.sleep(1)
         page = response.read()
-        hits = re.findall(patten, page)[0]
-        print "Done:%s" % hits
-        results.append([stock, hits.replace(',','')])
-        time.sleep(1)
+        hit = re.findall(patten, page)[0]
+        print "Done:%s" % hit
+        sql = 'INSERT INTO %s (id, hit) VALUES (%s, %s)' % (search_engine, stock, hit)
 
-writecsvfile(daily_out_file, results)
+conn.commit()
+conn.close()
 
