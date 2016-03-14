9e2af43bf790e51c40844728aaf115f718277556
Weirch Sodora <sodora@gmail.com>
2016-03-14 17:13:34 +0800

    web/show.php

--- web/show.php
+++ web/show.php
@@ -10,15 +10,22 @@ echo <<<END
 <title>{$_GET['title']}</title>
 <script type="text/javascript" src="http://cdn.bootcss.com/jquery/2.2.1/jquery.min.js"></script>
 <script type="text/javascript" src="http://cdn.bootcss.com/highstock/4.2.3/highstock.js"></script>
+<script type="text/javascript" src="http://cdn.bootcss.com/highcharts/4.2.3/modules/exporting.js"></script>
 <script type="text/javascript">
 Highcharts.setOptions({
-    global: {
-        lang: {
-            loading: '正在加载...',
-            months: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
-            shortMonths: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
-            weekdays: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期']
-        }
+    lang: {
+        printChart: '打印',
+        downloadJPEG: '导出JPG格式',
+        downloadPDF: '导出PDF格式',
+        downloadPNG: '导出PNG格式',
+        downloadSVG: '导出SVG格式',
+        loading: '正在加载...',
+        rangeSelectorZoom: '时间范围',
+        rangeSelectorFrom: '开始',
+        rangeSelectorTo: '结束',
+        months: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+        shortMonths: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+        weekdays: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期']
     }
 });
 $(function () {
@@ -73,7 +80,7 @@ $(function () {
 </script>
 </head>
 <body>
-<div id="container" style="height: 400px; min-width: 600px"></div>
+<div id="container" style="height: 600px; min-width: 600px"></div>
 </body>
 </html>
 END;
