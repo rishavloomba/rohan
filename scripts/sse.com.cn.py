321ff209dbdc7802341cd7db45c6d758ff8f3a9c
Weirch Sodora <sodora@gmail.com>
2016-03-17 11:13:41 +0800

    scripts/sse.com.cn.py

--- scripts/sse.com.cn.py
+++ scripts/sse.com.cn.py
@@ -5,13 +5,16 @@ import time
 
 dt = time.strftime("%Y%m%d")
 sqlite_file = 'sqlite/sse.com.cn.db'
-url = 'http://yunhq.sse.com.cn:32041/v1/sh1/snap/000001?select=open%2Chigh%2Clow%2Clast%2Cvolume%2Camount'
-sql = 'INSERT INTO szzs (open, high, low, close, volume, amount) VALUES ("%s","%s","%s","%s","%s","%s")'
+url = 'http://yunhq.sse.com.cn:32041/v1/sh1/dayk/000001?select=date%2Copen%2Chigh%2Clow%2Cclose%2Cvolume%2Camount&begin=-2&end=-1'
+sql = 'INSERT INTO szzs (dt,open,high,low,close,volume,amount) VALUES ("%s","%s","%s","%s","%s","%s","%s")'
 
-def insert_sqlite(entry):
+def insert_sqlite(entries):
     conn = sqlite3.connect(sqlite_file)
-    sqli = sql % (entry[0],entry[1],entry[2],entry[3],entry[4],entry[5])
-    conn.execute(sqli)
+    for entry in entries:
+        d = str(entry[0])
+        dat = d[0:4]+'-'+d[4:6]+'-'+d[6:8]
+        sqli = sql % (dat,entry[1],entry[2],entry[3],entry[4],entry[5],entry[6])
+        conn.execute(sqli)
     conn.commit()
     conn.close()
 
@@ -25,8 +28,8 @@ def parse_web():
             http = httplib2.Http()
             response, content = http.request(url)
             c = json.loads(content)
-            if dt == str(c['date']):
-                results = c['snap']
+            if dt == str(c['kline'][-1][0]):
+                results = c['kline']
             n = 10
         except:
             print n
