909422f3b4505ab1fc61f5ed9455ec4ba2b15fd8
Sodora <sodora@gmail.com>
2016-03-17 02:26:23 +0000

    scripts/sse.com.cn.py sqlite/sse.com.cn.db

--- scripts/sse.com.cn.py
+++ scripts/sse.com.cn.py
@@ -6,14 +6,12 @@ import time
 dt = time.strftime("%Y%m%d")
 sqlite_file = 'sqlite/sse.com.cn.db'
 url = 'http://yunhq.sse.com.cn:32041/v1/sh1/snap/000001?select=open%2Chigh%2Clow%2Clast%2Cvolume%2Camount'
-sql = 'INSERT INTO szzs (open, high, low, close, volume, amount) VALUES (%s)'
+sql = 'INSERT INTO szzs (open, high, low, close, volume, amount) VALUES ("%s","%s","%s","%s","%s","%s")'
 
-def insert_sqlite(entries):
+def insert_sqlite(entry):
     conn = sqlite3.connect(sqlite_file)
-    for entry in entries:
-        values = '"' + '", "'.join(entry) + '"'
-        sql = sql % (values)
-        conn.execute(sql)
+    sqli = sql % (entry[0],entry[1],entry[2],entry[3],entry[4],entry[5])
+    conn.execute(sqli)
     conn.commit()
     conn.close()
 
@@ -33,7 +31,7 @@ def parse_web():
         except:
             print n
             time.sleep(60)
-   return results
+    return results
 
 def main():
     insert_sqlite(parse_web())
