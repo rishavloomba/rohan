76b8bb615b9f4aeaed11d81e15bb329f3159639a
Weirch Sodora <sodora@gmail.com>
2016-05-13 18:27:01 +0800

    add baidu stat

--- web/login.php
+++ web/login.php
@@ -47,5 +47,14 @@ if(!isset($_SESSION['user'])){
     <?php echo '<span style="color: red">'.$error_msg.'</span>'; ?>
     </fieldset>
 </form>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?ee5755e6c222a7b48bb3afd1d06fd5bc";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
 </body>
 </html>
