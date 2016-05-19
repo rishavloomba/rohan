f5b7d717d4390b4d4e66077f50bec8fa2b583638
Weirch Sodora <sodora@gmail.com>
2016-05-19 20:44:05 +0800

    bd_stat url

--- web/common.php
+++ web/common.php
@@ -5,7 +5,7 @@ $bd_stat = '
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?ee5755e6c222a7b48bb3afd1d06fd5bc";
+  hm.src = "http://hm.baidu.com/hm.js?ee5755e6c222a7b48bb3afd1d06fd5bc";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
