23d65ecb089c3baf1e02f7c6911fe2902a2e63ec
Weirch Sodora <sodora@gmail.com>
2016-06-25 11:49:46 +0800

    order by dt asc

--- web/json.php
+++ web/json.php
@@ -7,11 +7,11 @@ $db = $_GET['db'];
 
 if(isset($_GET['key']) and $_GET['key'] != '') {
     $val = urldecode($_GET['val']);
-    $sql = "SELECT `dt`,`" . $_GET['col'] . "` FROM `" . $_GET['tb'] . "` WHERE " . $_GET['key'] . " = '" . $val . "'";
+    $sql = "SELECT `dt`,`" . $_GET['col'] . "` FROM `" . $_GET['tb'] . "` WHERE " . $_GET['key'] . " = '" . $val . "' ORDER BY `dt` ASC";
 } elseif(isset($_GET['nonzero']) and $_GET['nonzero'] != '') {
-    $sql = "SELECT `dt`,`" . $_GET['col'] . "` FROM `" . $_GET['tb'] . "` WHERE " . $_GET['col'] . " != '0' and " . $_GET['col'] . " != ''";
+    $sql = "SELECT `dt`,`" . $_GET['col'] . "` FROM `" . $_GET['tb'] . "` WHERE " . $_GET['col'] . " != '0' and " . $_GET['col'] . " != '' ORDER BY `dt` ASC";
 } else {
-    $sql = "SELECT `dt`,`" . $_GET['col'] . "` FROM `" . $_GET['tb'] . "`";
+    $sql = "SELECT `dt`,`" . $_GET['col'] . "` FROM `" . $_GET['tb'] . "` ORDER BY `dt` ASC";
 }
 
 if($user_valid and strpos($user_priv,$db) !== false) {
