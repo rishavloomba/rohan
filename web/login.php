61cc2c04fadc5bd1327982919ef88b32bee50b72
Sodora Weirch <sodora@gmail.com>
2017-02-09 14:56:05 +0000

    login.php

--- web/login.php
+++ web/login.php
@@ -36,14 +36,14 @@ if(!isset($_SESSION['user'])){
 <body>
 <div align="center">
 <form method="post" action="login.php">
-    <fieldset style="width:20em;">
+    <fieldset style="width:19em;">
         <legend>用户登录</legend>
         <table style="text-align:right;">
             <tr><td>用户名</td><td><input type="text" id="username" name="username" value="<?php if(isset($_POST['username'])){ echo $_POST['username']; } ?>" /></td></tr>
             <tr><td>密码</td><td><input type="password" id="password" name="password" /></td></tr>
             <tr><td></td><td><?php echo '<span style="color: red">'.$error_msg.'</span> &nbsp; '; ?><input type="submit" value="登录" name="submit"/></td></tr>
         </table>
-        <div align="left"><ul>
+        <div align="left"><ul style="list-style-type:circle;margin-left:-1em;">
         <li><span style="color: green">本站点提供各种金融数据分享</span></li>
         <li><span style="color: green">请使用IE9以上版本浏览器，或最新版Firefox、Chrome浏览器进行登录</span></li>
         <li><span style="color: green">获取用户和密码请扫二维码并关注微信公众号RohanKDD</span></li>
