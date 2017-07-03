af377fe2042590579170061fb6b87addc6fd88df
Sodora Weirch <sodora@gmail.com>
2017-07-03 04:44:41 +0000

    dailyupdate

--- web/admindb.php
+++ web/admindb.php
@@ -2,7 +2,7 @@
 function adminer_object() {
   class AdminerSoftware extends Adminer {
     function login($login, $password) {
-      return ($login == 'admin' && $password == 'starwars');
+      return (sha1($login) == 'd033e22ae348aeb5660fc2140aec35850c4da997' && sha1($password) == '327156ab287c6aa52c8670e13163fc1bf660add4');
     }
     function name() {
       return 'RohanKDD';
