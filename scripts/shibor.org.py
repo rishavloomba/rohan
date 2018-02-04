5b3bedd84be9e01539c68899eac54af35b174e89
Weirch Sodora <sodora@gmail.com>
2018-02-04 07:57:15 +0000

    dailyupdate

--- scripts/shibor.org.py
+++ scripts/shibor.org.py
@@ -8,8 +8,8 @@ if len(sys.argv) < 2:
 else:
     dt = sys.argv[1]
 
-url = ['http://www.shibor.org/shibor/web/html/downLoad.html?nameNew=Historical_Shibor_Data_2017.txt&nameOld=Shibor%CA%FD%BE%DD2017.txt&shiborSrc=http%3A%2F%2Fwww.shibor.org%2Fshibor%2F&downLoadPath=data',
-       'http://www.shibor.org/shibor/web/html/downLoad.html?nameNew=Historical_Shibor_Tendency_Data_2017.txt&nameOld=Shibor%BE%F9%D6%B5%CA%FD%BE%DD2017.txt&shiborSrc=http%3A%2F%2Fwww.shibor.org%2Fshibor%2F&downLoadPath=data']
+url = ['http://www.shibor.org/shibor/web/html/downLoad.html?nameNew=Historical_Shibor_Data_2018.txt&nameOld=Shibor%CA%FD%BE%DD2018.txt&shiborSrc=http%3A%2F%2Fwww.shibor.org%2Fshibor%2F&downLoadPath=data',
+       'http://www.shibor.org/shibor/web/html/downLoad.html?nameNew=Historical_Shibor_Tendency_Data_2018.txt&nameOld=Shibor%BE%F9%D6%B5%CA%FD%BE%DD2018.txt&shiborSrc=http%3A%2F%2Fwww.shibor.org%2Fshibor%2F&downLoadPath=data']
 
 sql = ['insert into shibor (dt,o_n,w1,w2,m1,m3,m6,m9,y1) values ("%s")',
        'insert into shibor_ma (dt,o_n_5,o_n_10,o_n_20,w1_5,w1_10,w1_20,w2_5,w2_10,w2_20,m1_5,m1_10,m1_20,m3_5,m3_10,m3_20,m6_5,m6_10,m6_20,m9_5,m9_10,m9_20,y1_5,y1_10,y1_20) values ("%s")']
